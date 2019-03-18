7450ff1408313e57072adbf4d664d6ca3d06070f
Jessica Ferguson <Jessica@explore-datascience.net>
2019-03-18 16:15:41 +0200

    recursion and sort attempt1

---
+++ mypackage/recursion.py
@@ -0,0 +1,46 @@
+def sum_array(array):
+
+    '''Return sum of all items in array'''
+
+    return sum(array)
+
+def fibonacci(n):
+
+    """
+    Calculate nth term in fibonacci sequence
+
+    Args:
+        n (int): nth term in fibonacci sequence to calculate
+
+    Returns:
+        int: nth term of fibonacci sequence,
+             equal to sum of previous two terms
+
+    Examples:
+        >>> fibonacci(1)
+        1
+        >> fibonacci(2)
+        1
+        >> fibonacci(3)
+        2
+    """
+
+    if n <= 1:
+        return n
+
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+def factorial(n):
+
+    '''Return n!'''
+    if n == 1:
+        return n
+    else:
+         return n * factorial(n-1)
+
+def reverse(word):
+
+    '''Return word in reverse'''
+    reversedstring = word[::-1]
+    return reversedstring
---
+++ mypackage/sorting.py
@@ -0,0 +1,31 @@
+def bubble_sort(items):
+    """ Implementation of bubble sort """
+    out = items.copy() # in place protection on items
+    for i in range(len(out)):
+        for j in range(len(out)-1-i):
+            if out[j] > out[j+1]:
+                out[j], out[j+1] = out[j+1], out[j]     # Swap!
+
+    return out
+
+def merge_sort(items):
+
+    '''Return array of items, sorted in ascending order'''
+
+    len_i = len(items)
+    # Base case. A list of size 1 is sorted.
+    # Cae returns, so if reached then function will terminate after line 8
+    if len_i == 1:
+        return items
+    # Recursive case. Find midpoint of list
+    mid_point = int(len_i / 2)
+    # divide list into two halves
+    i1 = merge_sort(items[:mid_point])
+    i2 = merge_sort(items[mid_point:])
+    # call merge_sort function on each half of list
+    return merge(i1, i2)
+
+def quick_sort(items):
+
+    '''Return array of items, sorted in ascending order'''
+    return sorted(items)
